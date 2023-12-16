07a2ec182bf6fb6f8d31cd3220d2e423a668ad4e
desh01prajakta <prajudeshmukh11@gmail.com>
2023-12-16 09:33:50 -0600

    commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 //import { useState } from 'react'
 
 import './App.css'
+import React from "react"; 
 
+export default function ContactList() { 
+
+  return ( 
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="3">Contact List</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+            </tr>
+            {
+               // Map over data here
+             }
+          </tbody>
+        </table>
+    ); 
+}
 
       
 
