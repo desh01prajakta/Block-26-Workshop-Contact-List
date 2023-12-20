b246bf01b0f7a9d4d6b19cacb2762563435c3dbc
desh01prajakta <prajudeshmukh11@gmail.com>
2023-12-19 20:47:53 -0600

    stage 3 complete

--- src/components/contactList.jsx
+++ src/components/contactList.jsx
@@ -1,6 +1,7 @@
 import React from "react"; 
 import { useState } from 'react'
 import ContactRow from "./contactRow";
+import { useEffect } from "react";
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -11,9 +12,21 @@ const dummyContacts = [
 
 
 export default function ContactList() { 
-    const [contacts, setContacts] = useState(dummyContacts)
-    console.log("Contacts: ", contacts);
-
+    const [contacts, setContacts] = useState(dummyContacts);
+    useEffect(()=>{
+        async function fetchContacts(){
+            try{ 
+                const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+                const result = await response.json();
+                setContacts(result)
+            }
+            catch(error){
+                console.log(error)
+            }
+        }
+        fetchContacts()
+    },[])
+    console.log("Contacts: ", contacts)
   return ( 
         <table>
           <thead>
