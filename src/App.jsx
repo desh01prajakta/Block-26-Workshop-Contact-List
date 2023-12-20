8b9fc06e2c0c9fb641cc578c47eb704b9b9a919e
desh01prajakta <prajudeshmukh11@gmail.com>
2023-12-19 20:59:01 -0600

    inprogress

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-//import { useState } from 'react'
-
+import { useState } from 'react'
 import React from 'react';
 import './App.css';
 import ContactList from './components/contactList';
+const [selectedContactId, setSelectedContactId] = state(null);
 
 export default function App() {
   
